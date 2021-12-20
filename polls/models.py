b8d9c30fc453033d77ce0d201fbc25312ee2de78
Sirisha Konda <palakrai0108@bitbucket.org>
2021-12-20 16:27:21 +0530

    added str function to the Question class in models

--- polls/models.py
+++ polls/models.py
@@ -7,6 +7,10 @@ class Question(models.Model):
     pub_date = models.DateTimeField('date published')
 
 
+    def __str__(self):
+        return self.question_text
+
+
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
